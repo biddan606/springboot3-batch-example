c951d3685579a8235fdedb86a53912c3bbd01954
biddan606 <biddan606@gmail.com>
2024-12-21 07:13:57 +0900

    프로젝트 생성

---
+++ src/test/java/dev/biddan/springbootbatchexample/ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.biddan.springbootbatchexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
